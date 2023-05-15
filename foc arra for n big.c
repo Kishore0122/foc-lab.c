2212dc05e11387443fd6648be58b28b8949bc5ff
Kishore0122 <129872649+Kishore0122@users.noreply.github.com>
2023-05-15 20:42:15 +0530

    Add files via upload

---
+++ foc arra for n big.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int n,i;
+  int arr[100];
+  printf("Enter the number of elements \n: ");
+  scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+    printf("Enter number %d: ", i);
+    scanf("%d", &arr[i]);
+  }
+    if (arr[0]== arr[i]) {
+      arr[0] == arr[i];
+    }
+  printf("%d is biggest",arr[n]);
+  return 0;
+}
---
+++ foc arra for search.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+  int n,key,i;
+  int arr[100];
+  printf("Enter the number of elements \n: ");
+  scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+    printf("Enter number %d: ", i);
+    scanf("%d", &arr[i]);
+  }
+  printf("Enter the number to search \n ");
+  scanf("%d", &key);
+    for (int i = 1; i <= n; i++)
+    if (key == arr[i]) {
+      arr[0] = arr[i];
+  printf("the element is in %d position",i );
+  }
+  return 0;
+}
